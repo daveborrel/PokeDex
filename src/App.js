ad589e56317dc33e3cb5e0afc3b5bd4c287ec0a9
daveborrel <dborrel@student.ubc.ca>
2022-04-22 16:24:00 -0700

    Updated API Call and CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [jsonObject, setJsonObject] = useState('')
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/1')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setJsonObject(result.sprites.front_default);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>
+  } else if (!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div className="App">
+        <h1>Testing</h1>
+        <img src={jsonObject} alt='Not Loading' />
+      </div>
+    );
+  }
 }
 
 export default App;
