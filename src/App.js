b59b0030f7cbecc442185029f4493afbd7d3ce6d
daveborrel <dborrel@student.ubc.ca>
2022-04-24 19:39:27 -0700

    integrated materialsUI updated buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import { Card, CardContent, CardMedia } from '@mui/material';
+import { Typography } from '@mui/material';
+import { Grid } from '@mui/material';
+import { Stack } from '@mui/material';
+import { Button } from '@mui/material';
 
 function App() {
   const [error, setError] = useState(null)
   const [isLoaded, setIsLoaded] = useState(false)
-  const [jsonObject, setJsonObject] = useState('')
+  const [sprite, setSprite] = useState('')
+  const [name, setName] = useState('')
+  const [currentPokemon, setCurrentPokemon] = useState(1)
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon/1')
+    fetch(`https://pokeapi.co/api/v2/pokemon/${currentPokemon}`)
       .then(res => res.json())
       .then(
         (result) => {
           setIsLoaded(true);
-          setJsonObject(result.sprites.front_default);
+          setSprite(result.sprites.front_default);
+          setName(result.species.name);
         },
         (error) => {
           setIsLoaded(true);
           setError(error);
         }
       )
-  }, [])
+  }, [currentPokemon])
 
   if (error) {
     return <div>Error: {error.message}</div>
@@ -27,10 +35,53 @@ function App() {
     return <div>Loading...</div>
   } else {
     return (
-      <div className="App">
-        <h1>Testing</h1>
-        <img src={jsonObject} alt='Not Loading' />
-      </div>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: '100vh' }}
+      >
+        <Grid item xs={3}>
+          <Card variant='outlined'>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div" align='center' >
+                {name}
+              </Typography>
+              <CardMedia
+                component="img"
+                height="300"
+                image={sprite}
+                alt="Loading" />
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid>
+          <Stack spacing={2} direction='row'>
+            <Button onClick={() => {
+              let temp = currentPokemon - 1;
+              if (temp < 0) {
+                temp = 1;
+              } else {
+                setCurrentPokemon(temp);
+              }
+            }}
+              variant="contained">Previous</Button>
+            <Button onClick={() => {
+              let temp = currentPokemon + 1;
+              if (temp > 898) {
+                temp = 898;
+              } else {
+                setCurrentPokemon(temp);
+              }
+            }}
+              variant="contained">Next</Button>
+          </Stack>
+        </Grid>
+
+      </Grid>
     );
   }
 }
